e5f3e1a1478073a29e20ba89c4af64c57cab244c
cbmarie255 <court.b1993@gmail.com>
2022-08-30 09:49:21 -0700

    Another version of the answer compatible with the checker

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i <= 9:
-        print("0{}, ".format(i), end='')
-    elif i > 9 and i < 99:
-        print("{}, ".format(i), end='')
-print("99")
+    if i < 99:
+        print("{:02d}, ".format(i), end='')
+    if i == 99:
+        print("99")
