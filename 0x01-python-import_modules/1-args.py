9f315a8010aa5a407f58e92c26fdd320bea50c80
cbmarie255 <court.b1993@gmail.com>
2022-08-31 10:02:27 -0700

    Name/Main add

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
-count = 0
-args = len(argv)
-if args - 1 == 0:
-    print("0 arguments.")
-if args - 1 == 1:
-    print("1 argument:")
-elif args - 1 != 0 and args - 1 != 1:
-    print("{} arguments:".format(args - 1))
-for i in range(1, args):
-    count = count + 1
-    print("{}: {}".format(count, argv[i]))
+if __name__ == "__main__":
+    count = 0
+    args = len(argv)
+    if args - 1 == 0:
+        print("0 arguments.")
+    if args - 1 == 1:
+        print("1 argument:")
+    elif args - 1 != 0 and args - 1 != 1:
+        print("{} arguments:".format(args - 1))
+    for i in range(1, args):
+        count = count + 1
+        print("{}: {}".format(count, argv[i]))
