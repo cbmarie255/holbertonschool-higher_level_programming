410442e6973e220cc6ecedc8568ad2a7ae416e0e
cbmarie255 <court.b1993@gmail.com>
2022-09-01 10:15:45 -0700

    Added condition where the list does not exist

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -5,4 +5,5 @@ def replace_in_list(my_list, idx, element):
     if idx not in range(len(my_list)):
         return my_list
     my_list[idx] = element
-    return my_list
+    my_list = new_list
+    return new_list
