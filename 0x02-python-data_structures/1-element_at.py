1bd3cc13e4b97c7983d6b709a4296111ffb917bf
cbmarie255 <court.b1993@gmail.com>
2022-09-01 09:01:12 -0700

    Adjusting range function

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if idx not in range(0, (len(my_list))):
+    if idx not in range(len(my_list)):
         return None
     for i in my_list:
         if i == idx:
