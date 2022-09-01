c7fa079400712f123ad49a9311c81a8c0c4b8f4b
cbmarie255 <court.b1993@gmail.com>
2022-09-01 08:57:02 -0700

    Finding an element at a given index

---
+++ 0x02-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx not in range(0, (len(my_list) + 1)):
+        return None
+    for i in my_list:
+        if i == idx:
+            return i
