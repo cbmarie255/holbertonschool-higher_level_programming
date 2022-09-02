e7836f800021b57a816a86f6b12425c4193f3f71
cbmarie255 <court.b1993@gmail.com>
2022-09-01 19:44:04 -0700

    well.. that was a bad idea lol

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    if len(my_list) == 0
-        return None
     for i in range(len(my_list)):
         if i % 2 == 0:
             new_list.append(True)
