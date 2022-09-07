af62fd7002fc0882cbec2996cd423e88776ae883
cbmarie255 <court.b1993@gmail.com>
2022-09-07 15:21:39 -0700

    Defining a class that will return the squared area

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Defining class Square with conditions and a public instance method"""
+
+
+class Square:
+    """defining a class named Square"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """will return the current square area"""
+        squared = self.__size**2
+        return squared
+
