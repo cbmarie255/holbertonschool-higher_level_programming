e5ec1f1105f7a5d812ba8ab70cc6ea6e7cb53975
cbmarie255 <court.b1993@gmail.com>
2022-09-06 20:05:40 -0700

    Pep8 and edgecase added, provided by prompt

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,10 @@
 def roman_to_int(roman_string):
     romanNumerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     conversion = 0
+    if type(roman_string) != str or not roman_string:
+        return 0
     for i, letter in enumerate(roman_string):
-        if (i + 1) == len(roman_string) or romanNumerals[letter] >= romanNumerals[roman_string[i + 1]]:
+        if (i+1) == len(roman_string) or romanNumerals[letter] >= romanNumerals[roman_string[i+1]]:
             conversion += romanNumerals[letter]
         else:
             conversion -= romanNumerals[letter]
