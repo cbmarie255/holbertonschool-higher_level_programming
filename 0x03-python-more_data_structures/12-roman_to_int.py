984376d63aa8b0953c86a63335d25f8e6d1908c9
cbmarie255 <court.b1993@gmail.com>
2022-09-06 20:17:03 -0700

    PEP8

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    romanNumerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    romanNs = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     conversion = 0
     if type(roman_string) != str or not roman_string:
         return 0
     for i, letter in enumerate(roman_string):
-        if (i+1) == len(roman_string) or romanNumerals[letter] >= romanNumerals[roman_string[i+1]]:
-            conversion += romanNumerals[letter]
+        if (i+1) == len(roman_string):
+            conversion += romanNs[letter]
+        elif romanNs[letter] >= romanNs[roman_string[i+1]]:
+            conversion += romanNs[letter]
         else:
-            conversion -= romanNumerals[letter]
+            conversion -= romanNs[letter]
     return conversion
