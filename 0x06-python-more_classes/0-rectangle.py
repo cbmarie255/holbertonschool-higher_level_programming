ceccee5cd34462e5a06b627a30e53fe53a5a3e48
cbmarie255 <court.b1993@gmail.com>
2022-09-13 16:40:32 -0700

    Extra if guards for missed checks

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -8,8 +8,17 @@ class Rectangle:
     """Creating a class for the rectangle"""
     def __init__(self, width=0, height=0):
         """Instantation to create objects"""
-        self.__width = width
-        self.__height = height
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        elif type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__width = width
+            self.__height = height
 
     @property
     def width(self):
@@ -21,7 +30,7 @@ class Rectangle:
         """Setter to set the width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -35,6 +44,6 @@ class Rectangle:
         """Setter to set the height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
