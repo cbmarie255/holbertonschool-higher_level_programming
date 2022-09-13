d3d349b6ce0168de1ed674a21a4684f037e2eb09
cbmarie255 <court.b1993@gmail.com>
2022-09-13 16:24:30 -0700

    Writing a class that defines a rectangle and raises errors when applicable

---
+++ 0x06-python-more_classes/0-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """Creating a class for the rectangle"""
+    def __init__(self, width=0, height=0):
+        """Instantation to create objects"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter to retrieve the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter to set the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter to retrieve the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter to set the height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
