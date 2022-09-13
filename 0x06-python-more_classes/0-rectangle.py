d5bde0333fa509cfc6d30aa3f58d2e41f6cfe2c7
cbmarie255 <court.b1993@gmail.com>
2022-09-13 16:30:32 -0700

    Switched the if statements, processor probably got confused with elif

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -21,10 +21,9 @@ class Rectangle:
         """Setter to set the width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -36,7 +35,6 @@ class Rectangle:
         """Setter to set the height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
