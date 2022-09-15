5dd0a4e432c1e323beb08bc5f2d358ebcd27372f
cbmarie255 <court.b1993@gmail.com>
2022-09-15 12:27:43 -0700

    PEP8 updates

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+    Returns the list of available attributes and methods of an object.
+"""
+
+
 def lookup(obj):
+    """Defining a fuction to lookup and object"""
     return dir(obj)
