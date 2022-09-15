836c015cab0cf81628392a4c6b7c5dc514c9bb71
cbmarie255 <court.b1993@gmail.com>
2022-09-15 14:04:06 -0700

    Checking for inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Returns True is the object is an instance of a class that inherited
+"""
+def inherits_from(obj, a_class):
+    """Checks for instance of a subclass"""
+    if type(obj) is a_class:
+        return False
+    if issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
