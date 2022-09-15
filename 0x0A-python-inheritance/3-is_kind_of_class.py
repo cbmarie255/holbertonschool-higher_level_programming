b26870b670a9e342ae9c088cee9850af88092fcc
cbmarie255 <court.b1993@gmail.com>
2022-09-15 13:28:18 -0700

    Boolean for an instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    Returns True if the object is an instance of, or the object is an instance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks for instance"""
+    return isinstance(obj, a_class)
