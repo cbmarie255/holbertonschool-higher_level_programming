1d78fa9a89aedcdab84c56a1d25568d5e7a232ff
cbmarie255 <court.b1993@gmail.com>
2022-09-19 12:13:59 -0700

    PEP8 changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@
 def read_file(filename=""):
     """will read the file and print to stdout"""
     with open(filename, 'r', encoding="UTF8") as f:
-        print(f.read(), end ='')
+        print(f.read(), end='')
