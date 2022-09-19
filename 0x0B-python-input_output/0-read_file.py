e6f4d5c5b01885bdd18b4566b370935b56521c7b
cbmarie255 <court.b1993@gmail.com>
2022-09-19 12:09:49 -0700

    took out th example as the filename and replaced it with a filename variable

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,5 @@
 
 
 def read_file(filename=""):
-    """Will read a textfile and prints it to stdout"""
-    with open('my_file_0.txt', 'r', encoding="UTF8") as f:
+    with open(filename, 'r', encoding="UTF8") as f:
         print(f.read(), end ='')
