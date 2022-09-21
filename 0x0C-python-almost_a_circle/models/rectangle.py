2b03f572fdae103f5013bf6787922dced53dae55
cbmarie255 <court.b1993@gmail.com>
2022-09-21 08:03:42 -0700

    Updating error message for checker

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -49,7 +49,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -73,3 +73,5 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+
