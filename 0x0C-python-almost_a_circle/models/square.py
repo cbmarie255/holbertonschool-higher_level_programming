73ef039c8efcff23888b3deca1759a5f9d8e23ab
cbmarie255 <court.b1993@gmail.com>
2022-09-23 08:05:18 -0700

    Building a square that inherits from rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Building a square.
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Inherited from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Super classing variables for the square from rectangle"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        message = "[Square] ({}) ".format(self.id, end='')
+        message2 = "{}/{} - ".format(self.x, self.y, end='')
+        message3 = "{}".format(self.width)
+        return message + message2 + message3
