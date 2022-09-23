c5acddf72765e5f8c586a7c1414ba17c7b9ee24c
cbmarie255 <court.b1993@gmail.com>
2022-09-23 12:13:42 -0700

    changing the empty string returned syntax

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,6 +20,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """changing json to string"""
         if list_dictionaries is None or len(list_dictionaries) < 0:
+            list_dictionaries = []
             return list_dictionaries
         else:
             return json.dumps(list_dictionaries)
