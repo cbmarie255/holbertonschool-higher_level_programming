394da527b8ce7de339cd262e04b879814a789d83
cbmarie255 <court.b1993@gmail.com>
2022-10-05 14:21:14 -0700

    typeo this whole time smh

--- 0x00-SQL_introduction/11-best_score.sql
+++ 0x00-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- will list all records wit a score >= 10 in second_table
 -- addding condition to lising
-SELECT score, name FROM second_list WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
