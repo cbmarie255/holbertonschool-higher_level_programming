4a45aab438b94216a03e4e1792d95fe3fd174da3
cbmarie255 <court.b1993@gmail.com>
2022-10-06 13:31:52 -0700

    Creating a table with a foreign key

---
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Creating a database and table including an foreign key.
+-- Database: hbtn_0d_usa; Table: cities; state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL FOREIGN KEY,
+	name VARCHAR(256) NOT NULL
+)
+AUTO_INCREMENT = 1;
