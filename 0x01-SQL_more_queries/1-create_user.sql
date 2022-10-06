21a46bda16fe577516057d1d143a24fc0ac07011
cbmarie255 <court.b1993@gmail.com>
2022-10-06 09:19:08 -0700

    Fixing syntax.

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Creates a MySQL server user who has al privileges, a password, and should not fail if the user exists.
 -- User: user_0d_1; Password: user_0d_1_pwd;
-CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
