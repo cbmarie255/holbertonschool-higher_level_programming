5481244194c52219772d69e0d8bb464307440a69
cbmarie255 <court.b1993@gmail.com>
2022-10-06 08:47:29 -0700

    Displaying all grants for given users on a given server.

---
+++ 0x01-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Listing all privileges of the MySQL users on a given server.
+-- Users: user_0d_1 and user_0d_2; Server: localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
