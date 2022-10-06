69cf0c5c7c9214af7fb40de63183aac1a5c1179d
cbmarie255 <court.b1993@gmail.com>
2022-10-06 09:54:59 -0700

    Making table attributes unique

---
+++ 0x01-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- Creating a table with a unique value.
+-- Table Name: unique_id; id INT with the default value 1 and must be unique; name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS unique_id (
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+);
