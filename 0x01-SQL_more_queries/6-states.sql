bb215d353dea27940e96324f3f85258f903f9614
cbmarie255 <court.b1993@gmail.com>
2022-10-06 10:07:07 -0700

    Adding value to AUTO GENERATED

--- 0x01-SQL_more_queries/6-states.sql
+++ 0x01-SQL_more_queries/6-states.sql
@@ -2,6 +2,7 @@
 -- Database: hbtn_0d_usa; Table: states; id INT unique, auto generated, can’t be null and is a primary key; name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
-);
+)
+AUTO_INCREMENT = 1;
