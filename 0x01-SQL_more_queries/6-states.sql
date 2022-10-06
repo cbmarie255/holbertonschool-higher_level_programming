e81f1b1a06f2f002d4bca63958b481534c900cb2
cbmarie255 <court.b1993@gmail.com>
2022-10-06 13:17:30 -0700

    Adding the database states is in

--- 0x01-SQL_more_queries/6-states.sql
+++ 0x01-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- Creating a database and table including auto generation.
 -- Database: hbtn_0d_usa; Table: states; id INT unique, auto generated, can’t be null and is a primary key; name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
+	name VARCHAR(256) NOT NULL,
 )
 AUTO_INCREMENT = 1;
