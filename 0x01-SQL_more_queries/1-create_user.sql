e5c2a8417b39c85f9025fe63e0eb709608e952ea
cbmarie255 <court.b1993@gmail.com>
2022-10-06 09:07:35 -0700

    Adding flush to update privileges immediately

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,5 @@
 -- User: user_0d_1; Password: user_0d_1_pwd;
 CREATE USER 'user_0d_1' IF NOT EXISTS
 IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1;
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'
+FLUSH PRIVILEGES;
