32e78d2f0b0498b662bcca0c74450ccb64f0f48e
cbmarie255 <court.b1993@gmail.com>
2022-10-06 09:45:46 -0700

    Syntax error

--- 0x01-SQL_more_queries/4-never_empty.sql
+++ 0x01-SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,6 @@
 -- Creating a table with a description.
 -- Table Name: id_not_null; id INT with the default value 1; name VARCHAR(256)
 CREATE TABLE IF NOT EXISTS id_not_null (
-	id INT DEFAULT 1
+	id INT DEFAULT 1,
 	name VARCHAR(256)
 );
