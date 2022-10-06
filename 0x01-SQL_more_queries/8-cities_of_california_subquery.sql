e5928936b4b1f45b1597535c05190ff37aa6fdb8
cbmarie255 <court.b1993@gmail.com>
2022-10-06 14:10:31 -0700

    Adding a nested fucntion to find the data for California

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Listing all the cities of Califorinia found on the database.
 -- nesting functions!
-SELECT id, name FROM hbtn_0d_usa.cities
-WHERE hbtn_0d_usa.states.name = Califorina
-ORDER BY cities.id ASC
+SELECT id, name FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
