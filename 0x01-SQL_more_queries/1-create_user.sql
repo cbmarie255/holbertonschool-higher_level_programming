9c59b21e4428e6f428540b1ce169a1d68160bb9c
cbmarie255 <court.b1993@gmail.com>
2022-10-06 09:13:43 -0700

    Deleting Flush, not needed

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
 -- Creates a MySQL server user who has al privileges, a password, and should not fail if the user exists.
 -- User: user_0d_1; Password: user_0d_1_pwd;
 CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS
-IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'
-FLUSH PRIVILEGES;
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
