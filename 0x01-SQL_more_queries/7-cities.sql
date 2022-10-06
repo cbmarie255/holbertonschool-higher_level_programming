5c4073a73c7b3f7496e312d6f5368791b3f7f428
cbmarie255 <court.b1993@gmail.com>
2022-10-06 13:39:18 -0700

    Changing the syntax of information, using REFERENCES

--- 0x01-SQL_more_queries/7-cities.sql
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -2,8 +2,10 @@
 -- Database: hbtn_0d_usa; Table: cities; state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL FOREIGN KEY (hbtn_0d_usa.state.id),
-	name VARCHAR(256) NOT NULL
+	id INT AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
 )
 AUTO_INCREMENT = 1;
