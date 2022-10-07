5aa906eec9206fd15c3a7a7d7545f27ac8a2a963
cbmarie255 <court.b1993@gmail.com>
2022-10-06 19:35:38 -0700

    Rearranging syntax... again

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,3 @@
 -- Lists all cities contained in the database with constraints.
 --Each record should display: cities.id - cities.name - states.name;
-SELECT cities.id, cities.name, states.name 
-FROM cities 
-INNER JOIN states ON cities.state_id=states.id;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;
