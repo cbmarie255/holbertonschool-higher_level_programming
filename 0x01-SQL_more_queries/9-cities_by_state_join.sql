347273870f9d2095a712d3497e5d9f272dd6f358
cbmarie255 <court.b1993@gmail.com>
2022-10-06 19:29:48 -0700

    Maybe ORDER BY is redundant?

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,4 @@
 --Each record should display: cities.id - cities.name - states.name;
 SELECT cities.id, cities.name, states.name 
 FROM cities 
-INNER JOIN states ON cities.state_id=states.id
-ORDER BY cities.id ASC;
+INNER JOIN states ON cities.state_id=states.id;
