a2102554e618b71526f3f1cabfdadb595c8f048a
cbmarie255 <court.b1993@gmail.com>
2022-10-06 19:46:27 -0700

    too much again?

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- Lists all cities contained in the database with constraints.
 --Each record should display: cities.id - cities.name - states.name;
 SELECT cities.id, cities.name, states.name FROM cities 
-LEFT JOIN states ON cities.state_id = states.id 
-ORDER BY cities.id ASC;
+LEFT JOIN states ON cities.state_id = states.id;
