f186dfe233a4731f6eaefd947abf73295caf942e
cbmarie255 <court.b1993@gmail.com>
2022-10-06 19:44:57 -0700

    syntax?

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities contained in the database with constraints.
 --Each record should display: cities.id - cities.name - states.name;
 SELECT cities.id, cities.name, states.name FROM cities 
-LEFT OUTER JOIN states ON cities.state_id=states.id 
+LEFT JOIN states ON cities.state_id = states.id 
 ORDER BY cities.id ASC;
