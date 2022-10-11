7d43c6205e083d43ce6422930e83a6c04f2036f6
cbmarie255 <court.b1993@gmail.com>
2022-10-10 17:58:59 -0700

    Importing the databse and inner joining.

---
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Database dump from hbtn_0d_tvshows to MySQL server
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genres.genre_id
+FROM tv_shows INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show.id=tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
