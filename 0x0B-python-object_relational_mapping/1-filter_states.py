fbbe1eff6fb6b2d3053f5bf04a049b32f5cc5f78
cbmarie255 <court.b1993@gmail.com>
2022-10-19 14:13:35 -0700

    Finding states in the database with given parameters.

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    A script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    args = argv
+    username = args[1]
+    password = args[2]
+    db_name = args[3]
+    data_base = MySQLdb.connect(host='localhost', user=username,
+                                passwd=password, db=db_name,
+                                port=3306)
+    cursor_jawn = data_base.cursor()
+    query = cursor_jawn.execute('''SELECT * FROM states
+                                WHERE states.name LIKE 'N%'
+                                ORDER BY states.id''')
+    row_to_tuple = cursor_jawn.fetchall()
+    for data in row_to_tuple:
+        print(data)
