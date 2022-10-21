73798ed797f5607cf22cd8757e69fcc7ccc54b8d
cbmarie255 <court.b1993@gmail.com>
2022-10-21 08:12:20 -0700

    Searching for keyword passed in and prints specific tuple listed by state id

---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    A script that takes in an argument and displays all values in the table
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    args = argv
+    username = args[1]
+    password = args[2]
+    db_name = args[3]
+    searched_state = args[4]
+    data_base = MySQLdb.connect(host='localhost', user=username,
+                                passwd=password, db=db_name,
+                                port=3306)
+    cursor_jawn = data_base.cursor()
+    query = cursor_jawn.execute('''
+                                SELECT * FROM states
+                                WHERE states.name = '{}'
+                                ORDER BY states.id ASC
+                                '''.format(searched_state))
+    row_to_tuple = cursor_jawn.fetchall()
+    for data in row_to_tuple:
+        print(data)
