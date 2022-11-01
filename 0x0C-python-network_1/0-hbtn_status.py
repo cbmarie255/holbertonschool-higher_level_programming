fc3b52f8ada292551037250a516147dbfe63b4a0
cbmarie255 <court.b1993@gmail.com>
2022-11-01 11:52:57 -0700

    maybe the tabs?

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         html = response.read()
         content = html.decode('utf-8')
         message_str = '''Body response:
-    \t- type: {}
-    \t- content: {}
-    \t- utf8 content: {}'''.format(type(html), html, content)
+        - type: {}
+        - content: {}
+        - utf8 content: {}'''.format(type(html), html, content)
         print(message_str)
