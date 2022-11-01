ac673655884a592381a36c0e6f6db1d44994d14a
cbmarie255 <court.b1993@gmail.com>
2022-11-01 11:33:48 -0700

    removing slash for output

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 """
 if __name__ == '__main__':
     import urllib.request
-    url = 'https://intranet.hbtn.io/status/'
+    url = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
         content = html.decode('utf-8')
