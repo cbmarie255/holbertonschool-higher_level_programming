c49363d52e01c240b831d58451203cbb2b23a96f
cbmarie255 <court.b1993@gmail.com>
2022-11-01 11:45:30 -0500

    Added decode for fetch and format string to print.

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -9,3 +9,7 @@ import urllib.request
 if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status/') as response:
         html = response.read()
+        content = html.decode('utf-8')
+        message_str = "Body response:\n \t - type: {}\n \t - content: {}\n \t
+                         - utf8 content: {}\n".format(type(html), html, content)
+        print(message_str)
