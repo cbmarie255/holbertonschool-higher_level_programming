32d99c86b651a38427711da5190b7c899bf55395
cbmarie255 <court.b1993@gmail.com>
2022-11-01 11:24:50 -0500

    Feching a given website using urllib package.

---
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    A script that fetches 'https://intranet.hbtn.io/status'.
+"""
+from urllib import response
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(https://intranet.hbtn.io/status) as response:
+        html = response.read()
