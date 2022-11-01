f284943fb61260ff63e4bb64ab6740ebe9d7b36f
cbmarie255 <court.b1993@gmail.com>
2022-11-01 16:11:22 -0500

    found most of the answer on the resource page smh!

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -3,13 +3,16 @@
     A script that takes in a URL and email and displays body of response.
 """
 import urllib.request
+import urllib.parse
 from sys import argv
-import requests
+
 
 if __name__ == '__main__':
     url = argv[1]
     email = argv[2]
-    email_obj = {'email': email}
-    posty = requests.post(url, data=email_obj)
-    with urllib.request.urlopen(posty) as response:
+    email_dict = {'email': email}
+    data = urllib.parse.urlencode(email_dict)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
