1ee6ef5a2a416fffa05d6cba5b08239f25cc12f7
cbmarie255 <court.b1993@gmail.com>
2022-12-01 12:38:24 -0800

    Forgot to run semistandard!

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let commandLineMessages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const commandLineMessages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i;
 for (i = 0; i < commandLineMessages.length; i++) {
   console.log(commandLineMessages[i]);
