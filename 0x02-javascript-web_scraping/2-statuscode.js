148dece73f9cb30154207e35cb0425abf71dc287
cbmarie255 <court.b1993@gmail.com>
2022-12-06 11:28:47 -0800

    Displaying the status code of a GET request where the url is passed in through the command line.

---
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (error != null) {
+    console.log(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
